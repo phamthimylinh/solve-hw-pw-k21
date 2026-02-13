693577e0d5b04b957e317340747d09f9abe91d57
phamthimylinh <phamthimylinhhaui@gmail.com>
2026-02-13 17:24:38 +0700

    lesson-6: javascritp part 4

---
+++ lesson-6/chore/move-old-code/01-linh/exercise/02-js-class-football.ts
@@ -0,0 +1,31 @@
+type Player = {
+    id: number;
+    name: string;
+    age: number;
+    position: string;
+}
+
+class Team {
+    name: string;
+    players: Player[];
+
+    constructor(name: string, players: Player[]) {
+        this.name = name;
+        this.players = players;
+    }
+
+    addPlayer(player: Player): void {
+        this.players.push(player);
+    }
+
+    listPlayers(): Player[] {
+        return this.players;
+    };
+}
+
+const team = new Team("Team 1", [
+    { id: 1, name: "Player 1", age: 20, position: "Forward" },
+    { id: 2, name: "Player 2", age: 21, position: "Midfielder" },
+]);
+team.addPlayer({ id: 3, name: "Player 3", age: 22, position: "Defender" });
+console.log(team.listPlayers());
